33ae2d4a219ec1374af5b261e999aa819628e3a2
agzaim <monikamiazga269@gmail.com>
2017-09-21 01:36:05 +0200

    problem solved

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 var express = require("express");
-var gameController = require("./controllers/gameController");
 
 var app = express();
-
-// set up template engine
-app.set("view engine", "ejs");
 
 
 // little middleware to get static files
 app.use(express.static("./public"));
+
+
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
+var serverAnswers = {"-1": "Your shot is too high", 1: "Your choice is too low", 0: "You got this! Gratulations!"};
+
+var mysteriousNumber = Math.floor(Math.random() * 100 + 1);
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+
+app.post("/", urlencodedParser, function(req, res) {
+
+    var diff = Math.sign(mysteriousNumber - req.body.guess);
+    res.send(serverAnswers[diff]);
+
+    // picking new number after player's correct guess
+    if (diff == 0) {
+        mysteriousNumber = Math.floor(Math.random() * 100 + 1);
+    }
+});
 
 
 // listen to port
@@ -16,5 +38,3 @@ app.listen(3000);
 console.log("now I'm listening to port 3000");
 
 
-// fire controller
-gameController(app);
--- public/assets/game.js
+++ public/assets/game.js
@@ -1,19 +1,49 @@
 $(function() {
+    var serverAnswerBox = $("#serverAnswer");
+    var playerAnswerInput = $("#userAnswer");
+    var renewButton = $("#renewButton");
+    var checkingButton = $("#checkingButton");
+
+
     $("form").on("submit", function(ev) {
         ev.preventDefault();
-        
-        var playerAnswerInput = $("#userAnswer");
+
         var playerAnswer = {guess:  parseInt(playerAnswerInput.val())};
-        
-//        var playerAnswer = parseInt($("#userAnswer").val());
-        console.log(playerAnswer);
-        
+
+
         $.ajax({
             type: "POST",
-            url: "/game",
-            data: playerAnswer          
+            url: "/",
+            data: playerAnswer,
+            success: function(result) {
+                serverAnswerBox.css("display", "block");
+                $("#serverAnswer").html(result);
+                if (result == "You got this! Gratulations!") {
+                    renewButton.css("display", "block");
+                    playerAnswerInput.css("display", "none");
+                    checkingButton.css("display", "none");
+                }
+            }
         })
+
     })
-    
+
+
+    // to show the player that server checking his proposition again
+    playerAnswerInput.on("focus", function() {
+        serverAnswerBox.css("display", "none");
+
+    })
+
+    // just for pretending that player actually "restart" server :D
+    renewButton.on("click", function() {
+        renewButton.css("display", "none");
+        serverAnswerBox.css("display", "none");
+        playerAnswerInput.css("display", "block");
+        playerAnswerInput.val("");
+        checkingButton.css("display", "block");
+
+    })
+
 });
 
